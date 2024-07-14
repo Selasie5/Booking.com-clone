1828c822aa3eba3542e3b74b542bb357c9099eba
SepenuSelasie <developerseps5@gmail.com>
2024-07-14 13:19:01 +0000

    Build header component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['static1.squarespace.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
